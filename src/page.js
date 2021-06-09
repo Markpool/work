fe614bb0d280d66cbe96424c26bc149ed8f24e29
Markpool <123546razil@mail.ru>
2021-06-09 20:11:42 +0400

    my commit

--- src/page.js
+++ src/page.js
@@ -2,31 +2,33 @@ import "./scss/main.scss";
 
 //точка входа
 
-$(document).ready(function() {
-  $('.header__burger').click(function(event) {
-    $('.header__burger, .header__menu').toggleClass('active-1');
-    $('body').toggleClass('lock');
-  });
 
 
- //open moadal window   
- let input = document.querySelector('.form__input-file');
- let placeHolder = document.querySelector('.form__input-file-placeholder');
- input.addEventListener('change', function () {
-   placeHolder.innerHTML = 'File upload';
-   placeHolder.style.color = '#7e7e7e';
- })
+// $(document).ready(function() {
+//   $('.header__burger').click(function(event) {
+//     $('.header__burger, .header__menu').toggleClass('active-1');
+//     $('body').toggleClass('lock');
+//   });
+
+
+//  //open moadal window   
+//  let input = document.querySelector('.form__input-file');
+//  let placeHolder = document.querySelector('.form__input-file-placeholder');
+//  input.addEventListener('change', function () {
+//    placeHolder.innerHTML = 'File upload';
+//    placeHolder.style.color = '#7e7e7e';
+//  })
  
- /*========================*/
- /*          form          */
- /*========================*/
+//  /*========================*/
+//  /*          form          */
+//  /*========================*/
  
- const form = document.querySelector('.modal-overlay'),
-  addBtn = document.querySelector('.add__btn'),
-  check = document.querySelector('.checkbox'),
-  formBtn = document.querySelector('.form_btn_unactive'),
-  sendBtn = document.querySelector('#send__form'),
-  closeBtn = document.querySelector('#close__form');
+//  const form = document.querySelector('.modal-overlay'),
+//   addBtn = document.querySelector('.add__btn'),
+//   check = document.querySelector('.checkbox'),
+//   formBtn = document.querySelector('.form_btn_unactive'),
+//   sendBtn = document.querySelector('#send__form'),
+//   closeBtn = document.querySelector('#close__form');
  
   
  
@@ -34,25 +36,28 @@ $(document).ready(function() {
  
  
  
- addBtn.addEventListener('click', function () {
- form.classList.add('form--open')
- document.body.style.overflow = "hidden";
- });
+//  addBtn.addEventListener('click', function () {
+//  form.classList.add('form--open')
+//  document.body.style.overflow = "hidden";
+//  });
  
- function closeForm () {
-   form.classList.remove('form--open');
-   document.body.style.overflow = "";
- };
+//  function closeForm () {
+//    form.classList.remove('form--open');
+//    document.body.style.overflow = "";
+//  };
  
- closeBtn.addEventListener('click', closeForm);
+//  closeBtn.addEventListener('click', closeForm);
  
  
- check.addEventListener('change', function () {
-   if (check.checked) {
-     formBtn.classList.add('form_btn_active');
-   } else {
-     formBtn.classList.remove('form_btn_active');
-   };
-  });
-});
+//  check.addEventListener('change', function () {
+//    if (check.checked) {
+//      formBtn.classList.add('form_btn_active');
+//    } else {
+//      formBtn.classList.remove('form_btn_active');
+//    };
+//   });
+
+  
+// });
+
 
